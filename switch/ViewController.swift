9d909880fa46e3d3f8de5824c0593cd725b1c592
Jared Spears <jspears2@zagmail.gonzaga.edu>
2019-09-12 21:21:42 -0700

    Initial Commit

---
+++ switch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  switch
+//
+//  Created by Jared Spears on 9/12/19.
+//  Copyright © 2019 Jared Spears. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
