343a39d2030da2d311a285685e32546aa5079345
Jared Spears <jspears2@zagmail.gonzaga.edu>
2019-09-12 21:25:41 -0700

    hi

--- switch/ViewController.swift
+++ switch/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var switchControl: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func switchPressed(_ sender: UISwitch) {
+        if switchControl.isOn {
+            self.view.backgroundColor = UIColor.white
+        } else {
+            self.view.backgroundColor = UIColor.black
+        }
+    }
+    
 }
 
